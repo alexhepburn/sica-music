ff095008d73a4549c8407c8421906b5c0bb39e81
alexhepburn <hepburnalexander@gmail.com>
2018-05-07 22:47:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt 
+from tqdm import tqdm
 
 sess = tf.InteractiveSession()
 
 n = 5
 m = 5
 k = 2
+lam = 0.8
 
 X = tf.placeholder(tf.float32, shape=(n,m))
 L = tf.placeholder(tf.float32, shape=(m,m))
 l = tf.placeholder(tf.float32, shape=())
 A = tf.Variable(np.random.rand(n,k), dtype=tf.float32)
 B = tf.Variable(np.random.rand(k,m), dtype=tf.float32)
-
-mat = tf.square(tf.norm((X-tf.matmul(A,B))))
-mat = tf.matmul(tf.matmul(tf.transpose(B), L), B)
-loss = tf.square(tf.norm((X-tf.matmul(A, B)), ord='euclidean')) - l * tf.matmul(tf.matmul(B, L), tf.transpose(B))
+loss = tf.square(tf.norm((X-tf.matmul(A, B)), ord='fro', axis=[0, 1])) - l * tf.trace(tf.matmul(tf.matmul(B, L), tf.transpose(B))) + 2*tf.norm(B, ord=2)
 
 x = [[0, 0, 1, 1, 1],
     [1, 1, 0, 0, 1],
@@ -32,7 +32,16 @@ La = [[1, -1, 0, 0, 0],
 #x = np.asarray(x)
 #L = np.asarray(L)
 
-opt = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+opt = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
 tf.global_variables_initializer().run()
-y = sess.run([opt, loss, mat], feed_dict={X:x, L:La, l:0.5})
-print(y[2])
+step = 1000
+losses = []
+for i in tqdm(range(0, step)):
+    y = sess.run([opt, loss], feed_dict={X:x, L:La, l:lam})
+    losses.append(y[1])
+plt.plot(range(0, step), losses)
+plt.show()
+print(np.matrix(x))
+print(tf.matmul(A,B).eval())
+np.save('A.npy', A.eval())
+np.save('B.npy', B.eval())
